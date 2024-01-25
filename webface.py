23cce76ef117b749c3aea84b44af13f1037261c3
Kubinjurica <jacobjurica69@gmail.com>
2024-01-25 12:16:52 +0100

    rozpracovane vzkazy

--- webface.py
+++ webface.py
@@ -13,7 +13,7 @@ import functools
 from sqlitewrap import SQLite
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlite3 import IntegrityError
-
+import datetime
 app = Flask(__name__)
 app.secret_key = b"totoj e zceLa n@@@hodny retezec nejlep os.urandom(24)"
 app.secret_key = b"x6\x87j@\xd3\x88\x0e8\xe8pM\x13\r\xafa\x8b\xdbp\x8a\x1f\xd41\xb8"
@@ -34,9 +34,31 @@ def pes():
 def krysa():
     return render_template("krysa.html")
 
-@app.route("/admin/")
-def admin():
-    return render_template("admin.html")
+@app.route("/vzkazy/", methods = ['GET'])
+def vzkazy():
+    if "user" not in session:
+        flash("Pouze pro přihlášené")
+        return redirect(url_for("login", url = request.path))
+    with SQLite("data.sqlite") as cursor:
+        response = cursor.execute("SELECT login, body, datetime FROM user JOIN message ON user.id = message.user_id ORDER BY datetime DESC")
+        response = response.fetchall()
+    return render_template("vzkazy.html", response = response)
+
+@app.route("/vzkazy/", methods = ['POST'])
+def vzkazy_post():
+    if "user" not in session:
+        flash("Pouze pro přihlášené")
+        return redirect(url_for("login", url = request.path))        
+    with SQLite("data.sqlite") as cursor:
+        response = cursor.execute("SELECT id FROM user WHERE login = ?", [session["user"]])
+        #response = response.fetchone()
+        user_id = list(response.fetchone())[0]
+
+    vzkaz = request.form.get('vzkaz')
+    if vzkaz:
+        with SQLite("data.sqlite") as cursor:
+            cursor.execute("INSERT INTO message (user_id, body, datetime) VALUES (?, ?, ?)", [user_id, vzkaz, datetime.datetime.now()])
+    return redirect(url_for("vzkazy"))
 
 @app.route("/login/", methods=["GET"])
 def login():
